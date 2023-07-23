115b17e3dfb8c7a326b00c6238c306cf33b1232b
John Watson <johnwatson484@hotmail.com>
2023-07-23 17:12:45 +0100

    Change wait

--- app/source/source.js
+++ app/source/source.js
@@ -57,7 +57,9 @@ const cacheScores = async (scores) => {
 
 const showScorers = async (driver) => {
   await driver.wait(until.elementLocated(By.css('.qa-match-block')), 2000)
-  await driver.findElement(By.className('qa-show-scorers-button')).click()
+  await driver.wait(until.elementLocated(By.css('.qa-show-scorers-button')), 10000)
+  await driver.wait(until.elementIsEnabled(driver.findElement(By.css('.qa-show-scorers-button'))), 10000)
+  await driver.findElement(By.css('.qa-show-scorers-button')).click()
   await driver.wait(until.elementLocated(By.css('.qa-match-block')), 2000)
 }
 
