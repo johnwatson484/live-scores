952a6760e756ec2d09a3815df94b9140a66e100b
John Watson <johnwatson484@hotmail.com>
2023-07-23 17:32:08 +0100

    Handle tracking modal

--- app/source/source.js
+++ app/source/source.js
@@ -29,7 +29,15 @@ const getScores = async () => {
   try {
     const date = moment().format('YYYY-MM-DD')
     await driver.get(`${endpoint}/${date}`)
-    await showScorers(driver)
+
+    try {
+      await showScorers(driver)
+    } catch (err) {
+      if (err.name === 'ElementClickInterceptedError') {
+        await closeModal(driver)
+        await showScorers(driver)
+      }
+    }
 
     const scores = []
 
@@ -63,36 +71,40 @@ const showScorers = async (driver) => {
   await driver.wait(until.elementLocated(By.css('.qa-match-block')), 2000)
 }
 
+const closeModal = async (driver) => {
+  await driver.findElement(By.xpath('//button[@title="I do not agree"]')).click()
+}
+
 const getMatches = async (matchElements, date, competition) => {
   const matches = []
   for (const matchElement of matchElements) {
-    // try {
-    const homeScore = await matchElement.findElement(By.className('sp-c-fixture__number--home')).getText()
-    const awayScore = await matchElement.findElement(By.className('sp-c-fixture__number--away')).getText()
+    try {
+      const homeScore = await matchElement.findElement(By.className('sp-c-fixture__number--home')).getText()
+      const awayScore = await matchElement.findElement(By.className('sp-c-fixture__number--away')).getText()
 
-    // If match postponed or abandoned then ignore
-    if (isLetter(homeScore[0])) {
-      continue
+      // If match postponed or abandoned then ignore
+      if (isLetter(homeScore[0])) {
+        continue
+      }
+
+      const homeTeam = await matchElement.findElement(By.className('sp-c-fixture__team-name--home'))?.findElement(By.css('abbr')).getAttribute('title')
+      const awayTeam = await matchElement.findElement(By.className('sp-c-fixture__team-name--away'))?.findElement(By.css('abbr')).getAttribute('title')
+
+      const match = {
+        date,
+        competition,
+        homeTeam,
+        awayTeam,
+        homeScore,
+        awayScore
+      }
+
+      match.goals = await getGoals(matchElement, homeTeam, awayTeam)
+
+      matches.push(match)
+    } catch (err) {
+      console.log(err)
     }
-
-    const homeTeam = await matchElement.findElement(By.className('sp-c-fixture__team-name--home'))?.findElement(By.css('abbr')).getAttribute('title')
-    const awayTeam = await matchElement.findElement(By.className('sp-c-fixture__team-name--away'))?.findElement(By.css('abbr')).getAttribute('title')
-
-    const match = {
-      date,
-      competition,
-      homeTeam,
-      awayTeam,
-      homeScore,
-      awayScore
-    }
-
-    match.goals = await getGoals(matchElement, homeTeam, awayTeam)
-
-    matches.push(match)
-    // } catch (err) {
-    //   console.log(err)
-    // }
   }
   return matches
 }
